572f798e588c3b137995df2c52f0f87f0b158a27
jrisdal <165679625+jrisdal@users.noreply.github.com>
2024-04-02 11:23:15 -0500

    Update heatmap.R
    Changed how WT AA is denoted and created function for easier creation of plot .png's

--- heatmap.R
+++ heatmap.R
@@ -3,7 +3,7 @@
 library(ggplot2)
 library(colorspace)
 library(reshape2)
-
+library(dplyr)
 
 #reference AA seq: MPPKEAPKKWKAPKGPKPTHRKNKNKLELGRAIKYARQKENAIEYLPDGEMRFTTDKHEANWVKLRSVTQESALDEFLSTAALADKDFTADRHSNVKIIRMDSGNDSATSQGFSMTNEQRGNLNAKQRALAKDLIVPRRPEWNEGMSKFQLDRQEKEAFLEWRRKLAHLQESNEDLLLTPFERNIEVWKQLWRVVERSDLVVQIVDARNPLLFRSVDLERYVKESDDRKANLLLVNKADLLTKKQRIAWAKYFISKNISFTFYSALRANQLLEKQKEMGEDYREQDFEEADKEGFDADEKVMEKVKILSIDQLEELFLSKAPNEPLLPPLPGQPPLINIGLVGYPNVGKSSTINSLVGAKKVSVSSTPGKTKHFQTIKLSDSVMLCDCPGLVFPNFAYNKGELVCNGVLPIDQLRDYIGPAGLVAERIPKYYIEAIYGIHIQTKSRDEGGNGDIPTAQELLVAYARARGYMTQGYGSADEPRASRYILKDYVNGKLLYVNPPPHLEDDTPYTREECEEFNKDLYVFDRLPDTRKEQVQNAAKAKGIDIVDLARDLNQLTFSAHTGGDTQKEAKSVTHGGKQAALYNAAEDLDRDFFKMNNVEGRLSTPFHKVQNSSAGKRHNKKNKSKNAKSKVFSIENN
 
@@ -25,14 +25,42 @@ desired_order = c("A","R","D","N","C","E","Q","G","H","I","L","K","M","F","P","S
 WT_csv$variable <- factor(WT_csv$variable, levels = desired_order) 
 
 
+wtseq <- "PPKEAPKKWKAPKGPKPTHRKNKNKLELGRAIKYARQKENAIEYLPDGEMRFTTDKHEANWVKLRSVTQESALDEFLSTAALADKDFTADRHSNVKIIRMDSGNDSATSQGFSMTNEQRGNLNAKQRALAKDLIVPRRPEWNEGMSKFQLDRQEKEAFLEWRRKLAHLQESNEDLLLTPFERNIEVWKQLWRVVERSDLVVQIVDARNPLLFRSVDLERYVKESDDRKANLLLVNKADLLTKKQRIAWAKYFISKNISFTFYSALRANQLLEKQKEMGEDYREQDFEEADKEGFDADEKVMEKVKILSIDQLEELFLSKAPNEPLLPPLPGQPPLINIGLVGYPNVGKSSTINSLVGAKKVSVSSTPGKTKHFQTIKLSDSVMLCDCPGLVFPNFAYNKGELVCNGVLPIDQLRDYIGPAGLVAERIPKYYIEAIYGIHIQTKSRDEGGNGDIPTAQELLVAYARARGYMTQGYGSADEPRASRYILKDYVNGKLLYVNPPPHLEDDTPYTREECEEFNKDLYVFDRLPDTRKEQVQNAAKAKGIDIVDLARDLNQLTFSAHTGGDTQKEAKSVTHGGKQAALYNAAEDLDRDFFKMNNVEGRLSTPFHKVQNSSAGKRHNKKNKSKNAKSKVFSIENN"
+wtseq <- strsplit(wtseq, "")[[1]] 
+WT_csv$wt_sequence <- wtseq #creates WT seq column for labeling of x axis
+
+View(WT_csv)
+
+##creates the first heatmap from codon 2 to 80, as it's only 79 codons long and so needs to be dealt with separately
+
 ggplot() +
   geom_tile(data = WT_csv, aes(x = factor(Codon), y = factor(variable), fill= as.numeric(score))) +
-  geom_point(data = subset(WT_csv, value == TRUE), aes(x = factor(Codon), y = factor(variable), color = "WT AA")) + # Add dots for WT sequences
-  scale_fill_gradient2(low= "brown", mid = "lightyellow", high= "darkgreen", midpoint = 1, name = "Fitness Scores") + #adds diverging color scale
-  scale_color_manual(name= "", values= "black") + # Add legend for dots
-  coord_fixed(ratio = 1, xlim = c(319, 479)) + #the codons that the figure includes (add 1 to each of them). Change depending on how big you want figure to be
-  scale_x_discrete(breaks = c(320, 340, 360, 380, 400, 420, 440, 460, 480)) + #adds the tickmarks (change every time)
-  labs(x = "Codon", y = "Amino Acid Substitution", title = "Heatmap of Amino Acid Substitution Fitness Scores in LSG1") +
-  theme(axis.text.y = element_text(size = 6))  #changes size of text on y axis 
+  scale_fill_gradient2(low= "brown", mid = "lightyellow", high= "darkgreen", midpoint = 1, name = "Fitness Scores") +
+  coord_fixed(ratio = 1, xlim = c(1,79)) +
+  scale_x_discrete(breaks = seq(2, 80, by=1), labels = paste(seq(2, 80, by=1), wtseq[1:79])) +
+  labs(x = "Position & WT AA", y = "Amino Acid", title = paste("Heatmap of Substitution Fitness Scores in LSG1 (Codons 2 to 80)")) +
+  theme(axis.text.y = element_text(size = 10),
+        axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
 
 
+#function to create all other heatmaps
+generate_heatmap <- function(start_codon) {
+  ggplot() +
+    geom_tile(data = WT_csv, aes(x = factor(Codon), y = factor(variable), fill= as.numeric(score))) +
+    scale_fill_gradient2(low= "brown", mid = "lightyellow", high= "darkgreen", midpoint = 1, name = "Fitness Scores") +
+    coord_fixed(ratio = 1, xlim = c(start_codon-1, start_codon + 79)) +
+    scale_x_discrete(breaks = seq(start_codon, start_codon + 80, by=1), labels = paste(seq(start_codon, start_codon + 80, by=1), wtseq[((start_codon)-1):(start_codon + 79)])) +
+    labs(x = "Position & WT AA", y = "Amino Acid", title = paste("Heatmap of Substitution Fitness Scores in LSG1 (Codons", start_codon, "to", start_codon + 80, ")")) +
+    theme(axis.text.y = element_text(size = 8),
+          axis.text.x = element_text(size = 8, angle = 90, vjust = 0.5, hjust = 1), #rotates x axis so that you can easily see AA and pos
+          axis.title = element_text(size=10), #changes size of axes labels
+          plot.title = element_text(size=12))        
+}
+
+
+#creates all other heatmaps and outputs them
+for (start_codon in seq(80, 560, by = 80)) {
+  filename <- paste("heatmap_codons_", start_codon, "_to_", start_codon + 80, ".png", sep = "")
+  heatmap <- generate_heatmap(start_codon)  # Generate heatmap
+  ggsave(filename, heatmap, width = 12, height = 6)  # Save the generated heatmap
+}
